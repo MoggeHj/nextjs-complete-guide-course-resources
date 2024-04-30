c3c49df8077b043f0fa23f590333ae884689ee87
Morgan HJELM <morgan.hjelm@soprasteria.com>
2024-04-30 13:31:53 +0200

    Client-side data fetching

--- code/03-routing-rendering/01-starting-project/app/(content)/news/page.tsx
+++ code/03-routing-rendering/01-starting-project/app/(content)/news/page.tsx
@@ -1,15 +1,53 @@
-import Link from "next/link";
+"use client";
 
-import { DUMMY_NEWS } from "@root/dummy-news";
+import { useEffect, useState } from "react";
+
 import NewsList from "@root/components/news-list";
+import exp from "constants";
 
-const newsPage = () => {
+const NewsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 };
 
-export default newsPage;
+export default NewsPage;
