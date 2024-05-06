427613a229954a2d21e5a31f628c394cbe87f925
Morgan HJELM <morgan.hjelm@soprasteria.com>
2024-05-06 10:40:30 +0200

    Add login logic and trigger different server actions via query parameters

--- code/08-authentication/01-starting-project/actions/auth-actions.ts
+++ code/08-authentication/01-starting-project/actions/auth-actions.ts
@@ -1,8 +1,8 @@
 "use server";
 import { redirect } from "next/navigation";
 
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
 import { create } from "domain";
 import { createAuthSession } from "@/lib/auth";
 
@@ -44,4 +44,39 @@ const signup = async (prevState, formData) => {
   }
 };
 
-export { signup };
+const login = async (prevState, formData) => {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+};
+
+const auth = async (mode, prevState, formData) => {
+  if (mode === "login") {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
+};
+
+export { signup, login, auth };
--- code/08-authentication/01-starting-project/app/page.tsx
+++ code/08-authentication/01-starting-project/app/page.tsx
@@ -1,7 +1,8 @@
 import AuthForm from "@/components/auth-form";
 
-const Home = async () => {
-  return <AuthForm />;
+const Home = async ({ searchParams }) => {
+  const formMode = searchParams.mode || "login";
+  return <AuthForm mode={formMode} />;
 };
 
 export default Home;
